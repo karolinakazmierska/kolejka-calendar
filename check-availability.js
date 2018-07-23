ee2c6b1009fca402ea24c9e391966bcfb6195ed9
Karolina Kazmierska <karo.a.kazmierska@gmail.com>
2018-07-23 16:48:17 +0200

    Removed unnecessary messages and printscreens

--- check-availability.js
+++ check-availability.js
@@ -16,28 +16,14 @@ casper.then(function() {
 casper.then(function() {
     this.click("a[data-activates='location-select']");
     console.log("Clicking on location selector");
-})
-
-casper.wait(3000, function() {
-    console.log('wait 3 seconds');
-});
-
-casper.then(function() {
-    this.capture('kolejka_step1.png');
+    this.wait(3000);
 })
 
 /* Click on Warsaw location */
 casper.then(function() {
     this.click("#location-select > li:nth-child(1) > a");
-    console.log("Clicking on Warsaw button")
-})
-
-casper.wait(3000, function() {
-    console.log('wait 3 seconds');
-});
-
-casper.then(function() {
-    this.capture('kolejka_step2.png');
+    console.log("Clicking on Warsaw button");
+    this.wait(3000);
 })
 
 /* Wait until the page is loaded and fill in the login form */
@@ -54,10 +40,6 @@ casper.waitWhileSelector('form#LoginForm', function(){
     console.log('Redirected to:', this.getTitle(), this.getCurrentUrl());
 })
 
-casper.then(function() {
-    this.capture('kolejka_step3.png');
-})
-
 /* Click on the queueInfo button */
 casper.then(function() {
     this.click("a[href='/rezerwacje/info/queueInfo']");
@@ -69,79 +51,42 @@ casper.waitWhileSelector("a[href='/rezerwacje/info/queueInfo']", function(){
     console.log('Redirected to:', this.getTitle(), this.getCurrentUrl());
 })
 
-casper.then(function() {
-    this.capture('kolejka_step4.png');
-})
-
 /* Select correct queue */
 casper.then(function() {
     this.click('.row.cards > div:nth-child(7) > .card.small > .card-action > a');
-
+    this.wait(3000);
 })
 
-/* Wait until the page with rules is loaded and click that you accept the regulations */
-casper.wait(3000, function() {
-    console.log('wait 3 seconds');
-});
-
+/* Click that you accept the regulations */
 casper.then(function() {
-    // this.click('#terms'); /* Did not work... */
     this.evaluate(function() {
         document.querySelector('#btn').classList.remove('disabled');
     })
-    console.log('Clicked on terms input checkbox')
-    console.log(this.getHTML('#customForm'))
-})
-
-casper.wait(3000, function() {
-    console.log('wait 3 seconds');
-});
-
-casper.then(function() {
-    this.capture('kolejka_step5.png');
+    console.log('Clicked on terms input checkbox');
+    this.wait(3000);
 })
 
 casper.then(function() {
     this.click('#btn');
+    this.wait(3000);
 })
 
-casper.wait(3000, function() {
-    console.log('wait 3 seconds');
-});
-
-casper.then(function() {
-    this.capture('kolejka_step6.png');
-})
-
-
-/* Now:
-- go to August
-- check if selected day has some class
-- if no --> check again
-- if yes --> break and trigger alarm!
-
- */
-
+/* Open calendar and check for available date */
 casper.thenOpen('https://kolejka-wsc.mazowieckie.pl/rezerwacje/pol/queues/200064/200084', function() {
     this.echo('Entering the calendar', this.getTitle(), this.getCurrentUrl())
     this.then(function () {
         function checkReload() {
-            this.echo('Firing function checkReload');
-            this.capture('kolejka_first-calendar.png')
+            this.echo('Checking...');
             this.click("div[id*='nav-next']");
-            this.echo('Clicked on the next arrow');
-            this.capture('kolejka_second-calendar.png')
             this.wait(3000);
             this.waitForSelector("div[id*='2018-08-14']", function() {
-                console.log('Waited for desired date to appear');
-                // var dateSlot = this.getElementAttribute("div[id*='2018-08-14']", 'class');
                 if (this.exists("div[id*='2018-08-14'].day.good")) {
                     this.echo('JEST!!!')
                     return;
                 } else {
                     this.echo('Date not available yet, checking again')
                     this.reload();
-                    this.wait(5000, checkReload)
+                    this.wait(10000, checkReload)
                 }
             });
 
@@ -149,23 +94,5 @@ casper.thenOpen('https://kolejka-wsc.mazowieckie.pl/rezerwacje/pol/queues/200064
         this.then(checkReload);
     })
 });
-
-// casper.waitForSelector('#btn', function() {
-//     console.log(document.querySelector('#btn').classList.remove('disabled'))
-// });
-//
-// casper.wait(3000, function() {
-//     console.log('wait 3 seconds');
-// });
-//
-//
-//
-
-//
-// casper.wait(3000, function() {
-//     console.log('wait 3 seconds');
-// });
-//
-
 
 casper.run();
